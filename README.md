3df4cafa1a985ae3f6f100f4fdd0b5b4d7dd95e0
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2022-08-09 15:57:39 +0200

    graphdb configuration

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# knowledge-graph
+# kFLOW Knowledge Graph
 A Knowledge Graph of Event and Relations
+
+
+## GraphDB
+
+Download latest version of GraphDB free by registering on the [product website](https://www.ontotext.com/products/graphdb/graphdb-free/). Place the obtained zip in the `graphdb` folder.
+We will use version 9.8.0.
+
+If you have the new Mac M1, run:
+
+    export DOCKER_DEFAULT_PLATFORM=linux/amd64  
+
+
+Then, run:
+
+    make free VERSION=9.8.0 -f graphdb/Makefile
+
+    docker-compose up -d
+
+> For more details look at the [graphdb-docker](https://github.com/Ontotext-AD/graphdb-docker#building-a-docker-image-based-on-the-free-edition) repository.
+
+Create the GraphDB repository
+
+    pip install -r scripts/requirements.txt
+    python3 scripts/create_repo.py
+
+
+Upload dumps
+
+    python3 scripts/load_dump.py hong
---
+++ scripts/load_dump.py
@@ -0,0 +1,63 @@
+import os
+from os import path
+import requests
+import argparse
+
+from db_utils import base, get_auth
+
+BASE_GRAPH = 'http://kflow.eurecom.fr'
+
+ROOT = './dump'
+INTERNAL_ROOT = '/opt/graphdb/home/graphdb-import'
+
+C_TYPE = {
+    'rdf': 'application/rdf+xml',
+    'owl': 'text/turtle',
+    'ttl': 'text/turtle'
+}
+
+
+def load_dump(name):
+    main_graph = path.join(BASE_GRAPH, name)
+    folder = path.join(ROOT, name)
+
+    # clear graph
+    headers = {'Authorization': get_auth()}
+    params = (('graph', main_graph),)
+
+    print('Deleting graph...')
+    response = requests.delete(f'{base}/repositories/kflow/rdf-graphs/service',
+                               headers=headers, params=params)
+    if response.status_code != 204:
+        print(response.status_code)
+        print(response.content)
+        return
+
+    print('Uploading new resources...')
+    # upload new resources
+    for filename in os.listdir(folder):
+        if not '.' in filename:
+            continue
+        name, ext = filename.rsplit('.', 2)
+        if ext in C_TYPE:
+            print('- ' + name)
+            headers['Content-Type'] = C_TYPE[ext]
+
+            with open(path.join(folder, filename), 'r', encoding='utf-8') as f:
+                data = f.read()
+
+            response = requests.post(f'{base}/repositories/kflow/rdf-graphs/service',
+                                     headers=headers, params=params, data=data.encode('utf-8'))
+            if response.status_code != 204:
+                print(response.status_code)
+                print(response.content)
+
+        else:
+            continue
+    print('completed')
+
+
+parser = argparse.ArgumentParser(description='Load dump in a graph.')
+parser.add_argument('name')
+args = parser.parse_args()
+load_dump(args.name)

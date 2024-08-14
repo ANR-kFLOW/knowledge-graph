8b51d9e04ca7a6262f6ede744e9406cd42223db0
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2024-08-14 11:50:52 +0200

    data split

--- kg_population/utils.py
+++ kg_population/utils.py
@@ -1,3 +1,12 @@
+import json
+import re
+import uuid
+from urllib.parse import urlparse
+
+import pandas as pd
+from rdflib import URIRef
+
+
 def node_creation(path, entity_mention, base_add=''):
     """
     This function generates a URI for an entity
@@ -19,9 +28,9 @@ def clean_text(text):
     """
 
     # Strip the last part of the text
-    index_of_last_occurence = text.rfind('</p><p>')
-    if index_of_last_occurence != -1:
-        text = text[:index_of_last_occurence]
+    index_of_last_occurrence = text.rfind('</p><p>')
+    if index_of_last_occurrence != -1:
+        text = text[:index_of_last_occurrence]
 
     text = re.sub(r"<.*?>", " ", text)  # Strip all the special characters in the text
 
@@ -45,11 +54,13 @@ def uri_validator(x):
 
 class MappingDict:
 
-    def __init__(self, encoding_dict={}):
+    def __init__(self, encoding_dict=None):
         """
         This function generates a dictionary used to lookup the unique ID's for words
         :param encoding_dict: A dictionary which already contains a mapping
         """
+        if encoding_dict is None:
+            encoding_dict = {}
         self.encoding_dict = encoding_dict
         self.max_key = self.get_max_key()
 
@@ -113,7 +124,7 @@ def gen_jointgt_input_format(data, output_file, encoding_dict=None, subj_col='su
     :return: the number of processed instances
     """
 
-    if encoding_dict == None:
+    if encoding_dict is None:
         encoding_dict = MappingDict()
         # encoding_dict.add_words(data[subj_col].to_list()) #Generate unique ID's per word (subject)
         encoding_dict.add_words(data[obj_col].to_list())  # Generate unique ID's per word (object)
@@ -132,10 +143,10 @@ def gen_jointgt_input_format(data, output_file, encoding_dict=None, subj_col='su
         # kbs[subj_id] = [row[subj_col], row[subj_col], [[relation, str(row[obj_col])]]] #For subject
         kbs[obj_id] = [row[obj_col], row[obj_col], [[relation, str(row[subj_col])]]]
 
-        if single_event == False:
+        if not single_event:
             json_dict = {"id": start_id + index,
                          "kbs": kbs,
-                         "text": [row[sent_col] if sent_col != None else None]}
+                         "text": [row[sent_col] if sent_col is not None else None]}
             full_data.append(json_dict)
             kbs = {}
 
@@ -152,111 +163,6 @@ def gen_jointgt_input_format(data, output_file, encoding_dict=None, subj_col='su
     return len(data)
 
 
-import re
-import uuid
-from urllib.parse import urlparse
-
-from rdflib import URIRef
-
-
-def node_creation(path, entity_mention, base_add=''):
-    """
-    This function generates a URI for an entity
-    :param path: A path that has to be added to the generated URI e.g. /sentence/
-    :param entity_mention: The entity for which the generation is done
-    :param base_add: Allows for more information to be added to the generated URI
-    :return:
-    """
-    base = f"http://kflow.eurecom.fr{base_add}"
-    uri = base + '/' + str(uuid.uuid5(uuid.NAMESPACE_DNS, path + entity_mention))
-    return URIRef(uri)
-
-
-def clean_text(text):
-    '''
-    This is used to remove the html codes from the text
-    :param text: The text to clean
-    :return: Cleaned text
-    '''
-
-    # Strip the last part of the text
-    index_of_last_occurence = text.rfind('</p><p>')
-    if index_of_last_occurence != -1:
-        text = text[:index_of_last_occurence]
-
-    text = re.sub(r"<.*?>", " ", text)  # Strip all the special characters in the text
-
-    text = text.strip()  # Remove the whitespace at the beginning, due to deletion
-
-    return text
-
-
-def uri_validator(x):
-    """
-    This function checks if a string is a URI/URI
-    :param x: The URI/ URL to check
-    :return:
-    """
-    try:
-        result = urlparse(x)
-        return all([result.scheme, result.netloc])
-    except:
-        return False
-
-
-class MappingDict:
-
-    def __init__(self, encoding_dict={}):
-        """
-        This function generates a dictionary used to lookup the unique ID's for words
-        :param encoding_dict: A dictionary which already contains a mapping
-        """
-        self.encoding_dict = encoding_dict
-        self.max_key = self.get_max_key()
-
-    def get_max_key(self):
-        if len(self.encoding_dict) != 0:
-            values = [int(value[1:]) for value in self.encoding_dict.values()]
-            return max(values)
-        else:
-            return 0
-
-    def increment_max_key(self):
-        self.max_key += 1
-
-    def add_words(self, words):
-
-        for word in words:
-            if word not in self.encoding_dict.keys():
-                self.encoding_dict[word] = f"W{str(self.max_key + 1)}"
-                self.increment_max_key()
-
-
-def gen_mapping_dict(*args):
-    import json
-    total_instances = 0
-    """
-    This function generates a dictionary containing all the unique keys for the objects in the JointGT files
-    :param args: paths to the jointGT files
-    :return: the mapping dictionary
-    """
-    encoding_dict = {}
-    filepaths = [*args]
-    for dataset in filepaths:
-        dataset = json.load(open(dataset))
-        total_instances += len(dataset)
-
-        for instance in dataset:
-
-            for key, value in instance['kbs'].items():
-                encoding_dict[value[0]] = key
-
-    encoding_dict = MappingDict(encoding_dict)
-    print(f"Processed {total_instances} instances")
-
-    return encoding_dict, total_instances
-
-
 def gen_jointgt_input_format_multiple(data, output_file, encoding_dict=None, subj_col='subject_values',
                                       rel_col='predicate', obj_col='object_values', sent_col=None, inst_col='instance'):
     """
@@ -270,11 +176,10 @@ def gen_jointgt_input_format_multiple(data, output_file, encoding_dict=None, sub
     :param obj_col: The column that contains the object
     :param sent_col: The column that contains the sentence
     :param inst_col: The instance that the row corresponds to, should be numbered from 0, and in order
-    :param start_id: the instance ID from which the count should start
     :return: the number of processed instances
     """
 
-    if encoding_dict == None:
+    if encoding_dict is None:
         encoding_dict = MappingDict()
         # encoding_dict.add_words(data[subj_col].to_list()) #Generate unique ID's per word (subject)
         encoding_dict.add_words(data[obj_col].to_list())  # Generate unique ID's per word (object)
@@ -316,7 +221,7 @@ def gen_jointgt_input_format_multiple(data, output_file, encoding_dict=None, sub
         relation = row[rel_col].split('/')[-1]
         # kbs[subj_id] = [row[subj_col], row[subj_col], [[relation, str(row[obj_col])]]] #For subject
         kbs[obj_id] = [row[obj_col], row[obj_col], [[relation, str(row[subj_col])]]]
-        sentence = row[sent_col] if sent_col != None else "None"
+        sentence = row[sent_col] if sent_col is not None else "None"
 
     with open(output_file, "w") as json_out:
 
